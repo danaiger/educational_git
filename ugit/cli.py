39c25cbd8b195304cf487ae61523e89b29fc6153
Dan <dan@dan.com>
2026-02-05 16:52:51 +0200

    generalize HEAD to ref

--- ugit/cli.py
+++ ugit/cli.py
@@ -48,6 +48,11 @@ def parse_args ():
     checkout_parser.set_defaults (func=checkout)
     checkout_parser.add_argument ('oid')
 
+    tag_parser = commands.add_parser ('tag')
+    tag_parser.set_defaults (func=tag)
+    tag_parser.add_argument ('name')
+    tag_parser.add_argument ('oid', nargs='?')
+
     return parser.parse_args ()
 
 
@@ -79,7 +84,7 @@ def commit (args):
 
 
 def log (args):
-    oid = args.oid or data.get_HEAD ()
+    oid = args.oid or data.get_ref ('HEAD')
     while oid:
         commit = base.get_commit (oid)
 
@@ -92,3 +97,8 @@ def log (args):
 
 def checkout (args):
     base.checkout (args.oid)
+
+
+def tag (args):
+    oid = args.oid or data.get_ref ('HEAD')
+    base.create_tag (args.name, oid)
