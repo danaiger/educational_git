88f33e8c4056f03b9b5f7b3397ebb76bbc55a456
Dan <dan@dan.com>
2026-02-08 17:19:35 +0200

    can use branch to print all branch names

--- ugit/cli.py
+++ ugit/cli.py
@@ -61,7 +61,7 @@ def parse_args ():
 
     branch_parser = commands.add_parser ('branch')
     branch_parser.set_defaults (func=branch)
-    branch_parser.add_argument ('name')
+    branch_parser.add_argument ('name', nargs='?')
     branch_parser.add_argument ('start_point', default='@', type=oid, nargs='?')
 
     status_parser = commands.add_parser ('status')
@@ -93,8 +93,14 @@ def read_tree (args):
     base.read_tree (args.tree)
 
 def branch (args):
-    base.create_branch (args.name, args.start_point)
-    print (f'Branch {args.name} created at {args.start_point[:10]}')
+    if not args.name:
+        current = base.get_branch_name ()
+        for branch in base.iter_branch_names ():
+            prefix = '*' if branch == current else ' '
+            print (f'{prefix} {branch}')
+    else:
+        base.create_branch (args.name, args.start_point)
+        print (f'Branch {args.name} created at {args.start_point[:10]}')
 
 def commit (args):
     print (base.commit (args.message))
