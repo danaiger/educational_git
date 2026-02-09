8f0dd46c6bfac5dcd83e094fd29cd086cc1e6670
Dan <dan@dan.com>
2026-02-09 14:19:43 +0200

    status shows diff from working dir to HEAD

--- ugit/cli.py
+++ ugit/cli.py
@@ -199,6 +199,13 @@ def status (args):
         print (f'On branch {branch}')
     else:
         print (f'HEAD detached at {HEAD[:10]}')
+    
+    print ('\nChanges to be committed:\n')
+    HEAD_tree = HEAD and base.get_commit (HEAD).tree
+    for path, action in diff.iter_changed_files (base.get_tree (HEAD_tree),
+                                                 base.get_working_tree ()):
+        print (f'{action:>12}: {path}')
+
 
 
 def reset (args):
