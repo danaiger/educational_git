a3edf824543bf4cc589a702c7cb50985ba0801b3
Dan <dan@dan.com>
2026-02-11 20:17:32 +0200

    status takes care of showing what is in index and ready to be commited

--- ugit/cli.py
+++ ugit/cli.py
@@ -227,13 +227,17 @@ def status (args):
     MERGE_HEAD = data.get_ref ('MERGE_HEAD').value
     if MERGE_HEAD:
         print (f'Merging with {MERGE_HEAD[:10]}')
-    
+
     print ('\nChanges to be committed:\n')
     HEAD_tree = HEAD and base.get_commit (HEAD).tree
     for path, action in diff.iter_changed_files (base.get_tree (HEAD_tree),
-                                                 base.get_working_tree ()):
+                                                 base.get_index_tree ()):
         print (f'{action:>12}: {path}')
 
+    print ('\nChanges not staged for commit:\n')
+    for path, action in diff.iter_changed_files (base.get_index_tree (),
+                                                 base.get_working_tree ()):
+        print (f'{action:>12}: {path}')
 
 
 def reset (args):
